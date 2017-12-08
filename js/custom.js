feea975fac1f36d960e775bc1974045f15b1a8a9
Huzaifa Mustafa <huzaifah.mustafa@gmail.com>
2017-12-08 13:07:07 +0500

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: zaifastafa
+ * @Date:   2017-11-23 20:46:01
+ * @Last Modified by:   zaifastafa
+ * @Last Modified time: 2017-12-07 20:08:40
+ */
+
+'use strict';
+
+$('.goto').on('click', function () {
+    var obj = $(this);
+    var element = obj.attr('data-target');
+    $("html, body").animate({
+        scrollTop: element ? $(element).offset().top - 70 : 0
+    }, 1000);
+});
