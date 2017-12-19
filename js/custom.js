3cd664b04be20fedcac239b430b6fb989d2ec258
Huzaifa Mustafa <huzaifah.mustafa@gmail.com>
2017-12-19 13:20:31 +0500

    - format code

--- js/custom.js
+++ js/custom.js
@@ -7,18 +7,18 @@
 
 'use strict';
 
-$('.goto').on('click', function () {
-    var obj = $(this);
-    var element = obj.attr('data-target');
-    $("html, body").animate({
-        scrollTop: element ? $(element).offset().top - 70 : 0
-    }, 1000);
+$('.goto').on('click', function() {
+  var obj = $(this);
+  var element = obj.attr('data-target');
+  $("html, body").animate({
+    scrollTop: element ? $(element).offset().top - 70 : 0
+  }, 1000);
 });
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 1024) {
-        $('#toTop').stop(true, true).fadeIn();
-    } else {
-        $('#toTop').stop(true, true).fadeOut();
-    }
+  if ($(this).scrollTop() > 1024) {
+    $('#toTop').stop(true, true).fadeIn();
+  } else {
+    $('#toTop').stop(true, true).fadeOut();
+  }
 });
