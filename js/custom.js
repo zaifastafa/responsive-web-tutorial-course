7b7f940dbbf47f7bd4b6658d3593ff9c57bd2366
Huzaifa Mustafa <huzaifah.mustafa@gmail.com>
2018-01-28 12:00:31 +0500

    - project changes
    - added img border
    - minor changes

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,20 @@
-/*
- * @Author: zaifastafa
- * @Date:   2017-11-23 20:46:01
- * @Last Modified by:   zaifastafa
- * @Last Modified time: 2017-12-07 20:08:40
- */
-
 'use strict';
 
 $('.goto').on('click', function() {
   var obj = $(this);
   var element = obj.attr('data-target');
-  $("html, body").animate({
-    scrollTop: element ? $(element).offset().top - 70 : 0
-  }, 1000);
+  $('html, body').animate(
+    {
+      scrollTop: element ? $(element).offset().top - 70 : 0
+    },
+    1000
+  );
 });
 
 $(window).scroll(function() {
   if ($(this).scrollTop() > 1024) {
-    $('#toTop').stop(true, true).fadeIn();
+    $('#toTop').fadeIn('slow');
   } else {
-    $('#toTop').stop(true, true).fadeOut();
+    $('#toTop').fadeOut('slow');
   }
 });
